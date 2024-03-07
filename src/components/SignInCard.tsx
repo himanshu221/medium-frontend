ff0c115235dc29c75bfe7a717ead7b9c82449b87
Himanshu <himanshubhushan775@gmail.com>
2024-03-08 01:19:31 +0530

    fixed toasters for signin and signup

--- src/components/SignInCard.tsx
+++ src/components/SignInCard.tsx
@@ -8,61 +8,89 @@ import { useRef } from "react"
 import axios, { AxiosResponse } from 'axios'
 import { backend_host } from "../config"
 import {SigninSchema} from "@himanshu212/medium-commons"
-import toast from "react-hot-toast"
+import toast, { Toaster } from "react-hot-toast"
 import { useNavigate } from "react-router-dom"
 
+type messageType  = string | {
+    issues : {
+        code: string,
+        message: string
+    }[]
+}
+
 export const SignInCard = () => {
-    const setUserName = useSetRecoilState(usernameAtom)
-    const navigate = useNavigate()
-    const emailInput = useRef<HTMLInputElement>(null)
-    const passwordInput = useRef<HTMLInputElement>(null)
+  const setUserName = useSetRecoilState(usernameAtom);
+  const navigate = useNavigate();
+  const emailInput = useRef<HTMLInputElement>(null);
+  const passwordInput = useRef<HTMLInputElement>(null);
 
+  async function signInHandler() {
+    if (emailInput.current && passwordInput.current) {
+      const body: SigninSchema = {
+        username: emailInput.current.value,
+        password: passwordInput.current.value,
+      };
+      const promise = axios
+        .post(`https://${backend_host}/api/v1/user/signin`, body, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+        .then((resp: AxiosResponse) => {
+          setUserName(emailInput.current ? emailInput.current.value : '');
+          localStorage.setItem('token', resp.data.token);
+          navigate('/dashboard');
+        });
 
-    async function signInHandler() {
-        if(emailInput.current && passwordInput.current){
-            const body : SigninSchema = {
-                username: emailInput.current.value,
-                password: passwordInput.current.value
-            }
-            const promise = axios.post(`https://${backend_host}/api/v1/user/signin`,body, {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
-                .then((resp: AxiosResponse) => {
-                    setUserName(emailInput.current ? emailInput.current.value : "")
-                    localStorage.setItem('token', resp.data.token)
-                    navigate('/dashboard')
-                }).catch(() => {
-                    alert('Invalid input')
-                })
-
-            toast.promise(promise,{
-                loading: "Saving user",
-                error: "Error occured",
-                success: null
-              },{
-                style: {
-                    minWidth: '250px',
-                    boxShadow: '0 0 5px rgba(0,0,0,0.3)',
-                    backgroundColor: 'rgb(10 10 10)',
-                    zIndex: '100'
-                },
-                error: {
-                  duration: 2000,
-                },
+      toast.promise(
+        promise,
+        {
+          loading: 'Signing in user',
+          error: err => {
+            const message : messageType = err.response.data.message
+            
+            if(typeof message === 'string')
+                return message
+            let acc: string = ""
+            message.issues.forEach(
+              (ele) => {
+                acc = acc.concat(ele.message, "\n")
               }
             )
+            return acc.substring(0,acc.length-2)
+          }
+          ,
+          success: "Successfully Sign in",
+        },
+        {
+          style: {
+            minWidth: '250px',
+            backgroundColor: '#18181b',
+            color: '#d4d4d8',
+          },
         }
+      );
     }
+  }
 
-    return <div className=" h-screen flex justify-center items-center">
-        <div className="h-[400px] w-[360px] bg-neutral-950 rounded-lg shadow-slate-800 shadow-[0_0_10px_2px_rgb(148,163,184)] flex flex-col items-center p-4">
-            <Heading label="Sign in" />
-            <InputBox type="text" placeholder={"Email"} refer={emailInput} />
-            <InputBox type="password" placeholder={"Password"} refer={passwordInput} />
-            <Button label="Sign in" onClickHandler={signInHandler}/>      
-            <BottomWarning text="Don't have an account?" link="Sign up" to="/signup"/>
-        </div>
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="h-[400px] w-[360px] bg-neutral-950 rounded-lg shadow-slate-800 shadow-[0_0_10px_2px_rgb(148,163,184)] flex flex-col items-center p-4">
+        <Heading label="Sign in" />
+        <InputBox type="text" placeholder={'Email'} refer={emailInput} />
+        <InputBox
+          type="password"
+          placeholder={'Password'}
+          refer={passwordInput}
+        />
+        <Button label="Sign in" onClickHandler={signInHandler} />
+        <BottomWarning
+          text="Don't have an account?"
+          link="Sign up"
+          to="/signup"
+        />
+        <Toaster />
+      </div>
     </div>
-}
+  );
+};
--- src/components/SignUpCard.tsx
+++ src/components/SignUpCard.tsx
@@ -1,18 +1,91 @@
+import { useSetRecoilState } from "recoil"
+import { usernameAtom } from "../store/atoms/user"
 import { BottomWarning } from "./BottomWarning"
 import { Button } from "./Button"
 import { Heading } from "./Heading"
 import { InputBox } from "./Input"
+import { useNavigate } from "react-router-dom"
+import { useRef } from "react"
+import { SignupSchema } from "@himanshu212/medium-commons"
+import axios, { AxiosResponse } from 'axios'
+import toast, { Toaster } from "react-hot-toast"
+import { backend_host } from "../config"
 
+type messageType  = string | {
+    issues : {
+        code: string,
+        message: string
+    }[]
+}
 export const SignUpCard = () => {
+    const setUserName = useSetRecoilState(usernameAtom);
+    const navigate = useNavigate();
+    const emailInput = useRef<HTMLInputElement>(null);
+    const firstname = useRef<HTMLInputElement>(null);
+    const lastname = useRef<HTMLInputElement>(null);
+    const passwordInput = useRef<HTMLInputElement>(null);
+
+    async function signUpHandler() {
+        if (emailInput.current && passwordInput.current && firstname.current && lastname.current) {
+            const body: SignupSchema = {
+              username: emailInput.current.value,
+              password: passwordInput.current.value,
+              firstname: firstname.current.value,
+              lastname: lastname.current.value
+            };
+            const promise = axios
+              .post(`https://${backend_host}/api/v1/user/signup`, body, {
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+              })
+              .then((resp: AxiosResponse) => {
+                setUserName(emailInput.current ? emailInput.current.value : '');
+                localStorage.setItem('token', resp.data.token);
+                navigate('/dashboard');
+              });
+      
+            toast.promise(
+              promise,
+              {
+                loading: 'Signing up user',
+                error: err => {
+                  const message : messageType = err.response.data.message
+                  
+                  if(typeof message === 'string')
+                      return message
+                  let acc: string = ""
+                  message.issues.forEach(
+                    (ele) => {
+                      acc = acc.concat(ele.message, "\n")
+                    }
+                  )
+                  return acc.substring(0,acc.length-2)
+                }
+                ,
+                success: "Successfully Sign up",
+              },
+              {
+                style: {
+                  minWidth: '250px',
+                  backgroundColor: '#18181b',
+                  color: '#d4d4d8',
+                },
+              }
+            );
+          }
+    }
+
     return <div className=" h-screen flex justify-center items-center">
         <div className="h-[540px] w-[360px] bg-neutral-950 rounded-lg shadow-slate-800 shadow-[0_0_10px_2px_rgb(148,163,184)] flex flex-col items-center p-4">
             <Heading label="Sign up" />
-            <InputBox type="text" placeholder={"Firstname"}/>
-            <InputBox type="text" placeholder={"Lastname"}/>
-            <InputBox type="text" placeholder={"Email"}/>
-            <InputBox type="password" placeholder={"Password"}/>
-            <Button label="Sign up"/>      
+            <InputBox refer={firstname} type="text" placeholder={"Firstname"}/>
+            <InputBox refer={lastname} type="text" placeholder={"Lastname"}/>
+            <InputBox refer={emailInput} type="text" placeholder={"Email"}/>
+            <InputBox refer={passwordInput} type="password" placeholder={"Password"}/>
+            <Button onClickHandler={signUpHandler} label="Sign up"/>      
             <BottomWarning text="Already have an account?" link="Sign in" to="/signin"/>
+            <Toaster />
         </div>
     </div>
 }
