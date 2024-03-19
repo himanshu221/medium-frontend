a89064ff10d442b00b7af64cc42aa6d498c98cc8
Himanshu <himanshubhushan775@gmail.com>
2024-03-20 02:03:27 +0530

    added active bg for editor buttons

--- src/components/EditorMenuBar.tsx
+++ src/components/EditorMenuBar.tsx
@@ -6,83 +6,84 @@ interface props {
 }
 
 export const MenuBar = ({editor} : props) => {
-    return <div className="flex gap-4 flex-wrap pt-3 pb-5 border-b border-neutral-600">
-        <button onClick={() => {
-            editor.chain().focus().toggleBold().run()
-        }}>
-            <Bold className="w-6 h-6 text-white" />
+    return <div className="flex gap-4 flex-wrap pb-5 border-b border-neutral-600">
+        <button 
+            onClick={() => {editor.chain().focus().toggleBold().run()}}
+            disabled={!editor.can().chain().focus().toggleItalic().run()}
+            className={editor.isActive("bold") ? "is-active" : ""}>
+            <Bold className="w-8 h-8 box-border p-1 text-white" />
         </button>
         <button
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        disabled={!editor.can().chain().focus().toggleItalic().run()}
-        className={editor.isActive("italic") ? "is-active" : ""}>
-            <Italic className="w-6 h-6 text-white" />
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            disabled={!editor.can().chain().focus().toggleItalic().run()}
+            className={editor.isActive("italic") ? "is-active" : ""}>
+            <Italic className="w-8 h-8 box-border p-1 text-white" />
         </button>
         <button
             onClick={() => editor.chain().focus().toggleStrike().run()}
             disabled={!editor.can().chain().focus().toggleStrike().run()}
             className={editor.isActive("strike") ? "is-active" : ""}>
-            <Strikethrough className="w-6 h-6 text-white" />
+            <Strikethrough className="w-8 h-8 box-border p-1 text-white" />
         </button>
         <button
             onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
             className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}>
-            <Heading1 className="w-6 h-6 text-white" />
+            <Heading1 className="w-8 h-8 box-border p-1 text-white" />
         </button>
         <button
             onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
             className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}>
-            <Heading2 className="w-6 h-6 text-white" />
+            <Heading2 className="w-8 h-8 box-border p-1 text-white" />
         </button>
         <button
             onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
             className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}>
-            <Heading3 className="w-6 h-6 text-white" />
+            <Heading3 className="w-8 h-8 box-border p-1 text-white" />
         </button>
         <button
             onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
             className={editor.isActive("heading", { level: 4 }) ? "is-active" : ""}>
-            <Heading4 className="w-6 h-6 text-white" />
+            <Heading4 className="w-8 h-8 box-border p-1 text-white" />
         </button>
         <button
             onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
             className={editor.isActive("heading", { level: 5 }) ? "is-active" : ""}>
-            <Heading5 className="w-6 h-6 text-white" />
+            <Heading5 className="w-8 h-8 box-border p-1 text-white" />
         </button>
         <button
             onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
             className={editor.isActive("heading", { level: 6 }) ? "is-active" : ""}>
-            <Heading6 className="w-6 h-6 text-white" />
+            <Heading6 className="w-8 h-8 box-border p-1 text-white" />
         </button>
         <button
             onClick={() => editor.chain().focus().toggleBulletList().run()}
             className={editor.isActive("bulletList") ? "is-active" : ""}>
-            <List className="w-6 h-6 text-white" />
+            <List className="w-8 h-8 box-border p-1 text-white" />
         </button>
         <button
             onClick={() => editor.chain().focus().toggleOrderedList().run()}
             className={editor.isActive("orderedList") ? "is-active" : ""}>
-            <ListOrdered className="w-6 h-6 text-white" />
+            <ListOrdered className="w-8 h-8 box-border p-1 text-white" />
         </button>
         <button
             onClick={() => editor.chain().focus().toggleCodeBlock().run()}
             className={editor.isActive("codeBlock") ? "is-active" : ""}>
-            <CodepenIcon className="w-6 h-6 text-white" />
+            <CodepenIcon className="w-8 h-8 box-border p-1 text-white" />
         </button>
         <button
             onClick={() => editor.chain().focus().toggleBlockquote().run()}
             className={editor.isActive("blockquote") ? "is-active" : ""}>
-            <Quote className="w-6 h-6 text-white" />
+            <Quote className="w-8 h-8 box-border p-1 text-white" />
         </button>
         <button
             onClick={() => editor.chain().focus().undo().run()}
             disabled={!editor.can().chain().focus().undo().run()}>
-            <Undo className="w-6 h-6 text-white" />
+            <Undo className="w-8 h-8 box-border p-1 text-white" />
         </button>
         <button
             onClick={() => editor.chain().focus().redo().run()}
             disabled={!editor.can().chain().focus().redo().run()}>
-            <Redo className="w-6 h-6 text-white" />
+            <Redo className="w-8 h-8 box-border p-1 text-white" />
         </button>
     </div>
 }
